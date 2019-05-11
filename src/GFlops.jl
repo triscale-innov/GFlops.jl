0b8441ca0ff1b16dd246857c8ffff37a57d2cfff
François Févotte <ff@triscale-innov.com>
2019-05-11 11:03:26 +0200

    Initial version

--- src/GFlops.jl
+++ src/GFlops.jl
@@ -1,3 +1,12 @@
 module GFlops
+import Statistics
+using BenchmarkTools:   @benchmark
+using InteractiveUtils: peakflops
+using Printf:           @printf
+
+export @gflops
+
+include("float.jl")
+include("gflops.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,24 @@
 using GFlops
 using Test
+
+
+function my_axpy!(a, x, y)
+    @inbounds @simd for i in eachindex(x)
+        y[i] += a*x[i]
+    end
+end
+
+using .GFlops
+
+let
+    N = 1000
+    x = rand(N)
+    y = rand(N)
+    @gflops my_axpy!(1.5, $x, $y)
+end
+
+let N = 100
+    x = rand(N,N)
+    y = rand(N)
+    @gflops \($x, $y)
+end
