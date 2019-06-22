a0ea5d5d79bb4989279b9a5b144e5cc5be1dcff7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-22 13:31:54 -0400

    alphabetized for easier care + feeding, `#` btwn 32s & 64s

--- src/overdub.jl
+++ src/overdub.jl
@@ -2,23 +2,43 @@ using Cassette
 
 Cassette.@context CounterCtx;
 
+const triops = (
+    (:fma32, Core.Intrinsics.fma_float, Float32),
+    (:muladd32, Core.Intrinsics.muladd_float, Float32),
+    #
+    (:fma64, Core.Intrinsics.fma_float, Float64),
+    (:muladd64, Core.Intrinsics.muladd_float, Float64),
+)
+
 const binops = (
+    (:abs32, Core.Intrinsics.abs_float, Float32),
     (:add32, Core.Intrinsics.add_float, Float32),
-    (:sub32, Core.Intrinsics.sub_float, Float32),
-    (:mul32, Core.Intrinsics.mul_float, Float32),
+    (:ceil32, Core.Intrinsics.ceil_llvm, Float32),
     (:div32, Core.Intrinsics.div_float, Float32),
+    (:floor32, Core.Intrinsics.floor_llvm, Float32),
+    (:mul32, Core.Intrinsics.mul_float, Float32),
+    (:rem32, Core.Intrinsics.rem_float, Float32),
+    (:sub32, Core.Intrinsics.sub_float, Float32),
+    (:trunc32, Core.Intrinsics.trunc_llvm, Float32),
+    #
+    (:abs64, Core.Intrinsics.abs_float, Float64),
     (:add64, Core.Intrinsics.add_float, Float64),
-    (:sub64, Core.Intrinsics.sub_float, Float64),
-    (:mul64, Core.Intrinsics.mul_float, Float64),
+    (:ceil64, Core.Intrinsics.ceil_llvm, Float64),
     (:div64, Core.Intrinsics.div_float, Float64),
+    (:floor64, Core.Intrinsics.floor_llvm, Float64),
+    (:mul64, Core.Intrinsics.mul_float, Float64),
+    (:rem64, Core.Intrinsics.rem_float, Float64),
+    (:sub64, Core.Intrinsics.sub_float, Float64),
+    (:trunc64, Core.Intrinsics.trunc_llvm, Float64),
 )
 
 const unops = (
     (:sqrt32, Core.Intrinsics.sqrt_llvm, Float32),
+    #
     (:sqrt64, Core.Intrinsics.sqrt_llvm, Float64),
 )
 
-const ops = Iterators.flatten((binops, unops)) |> collect
+const ops = Iterators.flatten((triops, binops, unops)) |> collect
 
 @eval mutable struct Counter
     $((:($(op[1]) ::Int) for op in ops)...)
